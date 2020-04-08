8ec99f8c1bd2e8f44acaa416a52e653fc2c7ffb3
Dharshana Ratnayake <dharshana.ratnayake@asb.co.nz>
2020-04-09 08:18:07 +1200

    increase turn speed

--- navigator/Robot.py
+++ navigator/Robot.py
@@ -6,7 +6,7 @@ class Robot:
     def __init__(self, host):
         self.host = host
         self.heading = 1500
-        self.turing_thrust = 1350
+        self.turing_thrust = 1250
         self.straight_thrust = 1400
         self.stop_thrust = 1500
         self.thrust = 1500
